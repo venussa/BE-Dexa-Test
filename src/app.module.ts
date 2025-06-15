c7a8b0635e4d4daa18d839bf008efdea68f06bce
venussa <yudha.romadhon.email@gmail.com>
2025-06-15 21:04:54 +0700

    feat: Create Auth API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmModuleConfig } from './config/db.config';
+import { AuthModule } from './auth/auth.module';
+import { AttendanceModule } from './attendance/attendance.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmModuleConfig),
+    AuthModule,
+    AttendanceModule,
+    UserModule,
+  ],
 })
-
 export class AppModule {}
