261ee2a29da2abca1f2517d496a887d20b0d536b
venussa <yudha.romadhon.email@gmail.com>
2025-06-15 21:06:11 +0700

    feat: Create Get Profile API

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import {  Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { User } from './decorators/user.decorator';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private userService: UserService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get('profile')
+    async getProfile(@User('id') userId: string) {
+        const user = await this.userService.getFullUserById(userId);
+        return user;
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepo: Repository<User>,
+      ) {}
+
+    async getFullUserById(id: string) {
+        const user = await this.userRepo.findOne({ where: { id } });
+        if (!user) throw new NotFoundException('User not found');
+
+        const { password, ...result } = user;
+        return result;
+    }
+}
