9bb7fbe8007de771998c73fe65d03901783d0db7
venussa <yudha.romadhon.email@gmail.com>
2025-06-15 21:51:53 +0700

    refactor: Add attendance status on Get Profile API

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/user.entity';
+import { Attendance } from '../attendance/attendance.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Attendance])],
   controllers: [UserController],
   providers: [UserService]
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,20 +1,37 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, Between } from 'typeorm';
+import { startOfDay, endOfDay } from 'date-fns';
 import { User } from './user.entity';
+import { Attendance } from '../attendance/attendance.entity';
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(User)
         private userRepo: Repository<User>,
+
+        @InjectRepository(Attendance)
+        private attendanceRepo: Repository<Attendance>
       ) {}
 
     async getFullUserById(id: string) {
-        const user = await this.userRepo.findOne({ where: { id } });
+        const user = await this.userRepo.findOne({ where: { id} });
         if (!user) throw new NotFoundException('User not found');
 
+        const today = new Date();
         const { password, ...result } = user;
-        return result;
+        const latestAttendance = await this.attendanceRepo.findOne({
+            where: {
+                user: { id },
+                timestamp: Between(startOfDay(today), endOfDay(today)),
+            },
+            order: { timestamp: 'DESC' },
+        });
+
+        return {
+            ...result,
+            attendanceStatus: latestAttendance?.type || 'NONE',
+        };
     }
 }
